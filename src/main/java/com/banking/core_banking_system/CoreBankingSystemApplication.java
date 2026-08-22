2499bc10456f591e1da8b31b8607f9a0ffde4310
toanbui-tech <buiminhtoan.cntt@gmail.com>
2026-08-22 13:40:59 +0700

    Initial commit: Spring Boot project scaffold

---
+++ src/main/java/com/banking/core_banking_system/CoreBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.banking.core_banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBankingSystemApplication.class, args);
+	}
+
+}
